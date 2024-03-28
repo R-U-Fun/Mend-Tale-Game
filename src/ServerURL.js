ff0b8203292c536d9ebea0b8c50f07969054135c
Aaroophan <arophn@gmail.com>
2024-03-28 21:17:03 +0530

    20240328_2116 - ServerURLS, NGROK

--- src/ServerURL.js
+++ src/ServerURL.js
@@ -1,11 +1,12 @@
 class Server {
 	MTServer1() {
-		return ("http://localhost:3214");
-		//return ("https://mend-tale-server1.onrender.com");
+		//return ("http://localhost:3214");
+		return ("https://mend-tale-server1.onrender.com");
 	}
 	MTServer2() {
-		return ("http://localhost:5000");
+		//return ("http://localhost:5000");
 		//return ("https://mend-tale-server1.onrender.com");
+		return ("https://dd0c-2402-4000-11c0-f7ec-e11e-cc14-95b3-d3f3.ngrok-free.app");
 	}
 }
 
