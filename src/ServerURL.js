7912f9cd6c8b2ae416613bbad21f533438787c72
Aaroophan <arophn@gmail.com>
2024-04-14 01:22:15 +0530

    20240414_0122 - OnRender

--- src/ServerURL.js
+++ src/ServerURL.js
@@ -1,7 +1,8 @@
 class Server {
 	MTServer1() {
 		//return ("http://localhost:3214");
-		return ("https://mend-tale-server1.onrender.com");
+		//return ("https://mend-tale-server1.onrender.com");
+		return ("https://mend-tale-server.onrender.com");
 	}
 	MTServer2() {
 		//return ("http://localhost:5000");
